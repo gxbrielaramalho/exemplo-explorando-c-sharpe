21b921b4692b66a04c94318fc8d46630f4c3bf0a
Gabriela Ramalho <gabrielathecreator@gmail.com>
2024-08-07 22:39:37 -0300

    tratamento para ToUpper

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -7,7 +7,22 @@ namespace exemplo_explorando_c_sharpe.Models
 {
     public class Pessoa
     {
-        public string Nome { get; set; }
+        private string _nome;
+        public string Nome 
+        { 
+            get {
+                return _nome.ToUpper();
+            }
+
+            set {
+                if (value == "") 
+                {
+                    throw new ArgumentException("O nome não pode ser vazio.");
+                }
+
+                _nome = value;
+            }
+        }
         public int Idade { get; set; }
 
         public void Apresentar() {
--- obj/Debug/net8.0/exemplo-explorando-c-sharpe.AssemblyInfo.cs
+++ obj/Debug/net8.0/exemplo-explorando-c-sharpe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exemplo-explorando-c-sharpe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f124b38f91d01967d2a1f48bdea24162a4d8695")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c2904ef8df95b057bdaaef2ba03bed6b1036162")]
 [assembly: System.Reflection.AssemblyProductAttribute("exemplo-explorando-c-sharpe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exemplo-explorando-c-sharpe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
